45bbb5639fd947c664ca08aa84570e92e25fa24c
Aneel Grewal <aneel.grewal@hotmail.com>
2024-05-20 08:32:31 +0200

    initial setup

---
+++ ConsoleApp3/MyClass.cs
@@ -0,0 +1,37 @@
+namespace ConsoleApp3;
+
+public class MyClass
+{
+    public readonly string Name;
+    private int intVal;
+
+    public int Val
+    {
+        get { return intVal;}
+        set
+        {
+            if (value >= 0 && value <= 10)
+                intVal = value;
+            else
+            {
+                throw (new ArgumentOutOfRangeException("Val", value, "Val must be assigned a value between 0 ans 10."));
+                
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {Name} \n Value: {Val}";
+    }
+
+    //private MyClass() : this("Default Name") { }
+    public MyClass(string newName)
+    {
+        Name = newName;
+        intVal = 0;
+    }
+
+    private int myDoubledInt = 5;
+    public int myDoubledIntProp => (myDoubledInt * 2);
+}
